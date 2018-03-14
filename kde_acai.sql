e845c68f0e229986df3767a956a219545884b87e
Wendell Cardoso <30928343+wendellcardoso@users.noreply.github.com>
2018-03-14 16:14:15 -0300

    Criação do Banco de Dados e das tabelas populadas

---
+++ kde_acai.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: kde_acai
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` longtext NOT NULL,
+  PRIMARY KEY (`id`,`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'wccs','Wendell','cardosowendell@gmail.com','senhawendell'),(2,'itc','Igor','torresigor@gmail.com','senhaigor'),(3,'lct','Leon','cruzleon@gmail.com','senhaleon');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lojas`
+--
+
+DROP TABLE IF EXISTS `lojas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lojas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `senha` longtext NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `avaliacao` float NOT NULL,
+  `localizacao` varchar(100) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `tipoFino` float NOT NULL,
+  `tipoMedio` float NOT NULL,
+  `tipoGrosso` float NOT NULL,
+  PRIMARY KEY (`id`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lojas`
+--
+
+LOCK TABLES `lojas` WRITE;
+/*!40000 ALTER TABLE `lojas` DISABLE KEYS */;
+INSERT INTO `lojas` VALUES (9,'acaidobom@gmail.com','senhaloja01','Açaí do Bom ',4.5,'Av. Dois Corações, 1245','91985284163',4,8,15),(10,'ilhaacai@gmail.com','senhaloja02','Açaí Da Ilha',5,'Rua Vasconcelos, 10','91987456329',5,9,15),(11,'julhoacai3@gmail.com','senhaloja03','Jullhos\'s Açaí ',3.3,'Av. Pobre Juan, 236','91985286394',3,7.5,14);
+/*!40000 ALTER TABLE `lojas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-14 16:07:00
