f9e083e92a59ff9e613c128f679560fd9866d8be
LeonCruz <leon.cruz.teixeira@gmail.com>
2018-03-14 17:45:24 -0300

    Criação das funções para manipulação do banco

--- KdeAcai/src/controler/ManipulacaoBanco.java
+++ KdeAcai/src/controler/ManipulacaoBanco.java
@@ -1,14 +1,97 @@
 package controler;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import controler.Conexao;
 
 public class ManipulacaoBanco {
+	public static Connection con;
+    public static Statement stm;
+    
 	public static void cadastrarCliente(String sql){
 		Conexao.conectar();
 		
+		try {
+			int resultado = stm.executeUpdate(sql);
+			System.out.println("Cadastro realizado com sucesso");
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao realizar operação");
+		}
 	}
 	
-	public static void cadasrtarLoja() {
+	public static void cadastrarLoja(String sql) {
 		Conexao.conectar();
+		
+		try {
+			int resultado = stm.executeUpdate(sql);
+			System.out.println("Cadastro realizado com sucesso");
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao realizar operação");
+		}
+	}
+	
+	public static void deletarCliente(String sql) {
+		Conexao.conectar();
+		
+		try {
+			int resultado = stm.executeUpdate(sql);
+			System.out.println("Cliente apagado");
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao realizar operação");
+		}
+	}
+	
+	public static void deletarLoja(String sql) {
+		Conexao.conectar();
+		
+		try {
+			int resultado = stm.executeUpdate(sql);
+			System.out.println("Loja apagada");
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao realizar operação");
+		}
+	}
+	
+	public static void atualizarDadosCliente(String sql) {
+		Conexao.conectar();
+		
+		try {
+			int resultado = stm.executeUpdate(sql);
+			System.out.println("Dados alterados com sucesso");
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao realizar operação");
+		}
+	}
+	
+	public static void atualizarDadosLoja(String sql) {
+		Conexao.conectar();
+		
+		try {
+			int resultado = stm.executeUpdate(sql);
+			System.out.println("Dados alterados com sucesso");
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao realizar operação");
+		}
+	}
+	
+	public static void pegarLojas(String sql) {
+		Conexao.conectar();
+		
+		try {
+			ResultSet resultado = stm.executeQuery(sql);
+			System.out.println("Dados alterados com sucesso");
+			
+		} catch (SQLException e) {
+			System.out.println("Erro ao realizar operação");
+		}
 	}
 }
