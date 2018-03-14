70fd5d5b8a48bc2dc1c53d673d777d74049041b3
LeonCruz <leon.cruz.teixeira@gmail.com>
2018-03-14 17:22:01 -0300

    Criação da classe para a conexão com o banco

--- KdeAcai/src/controler/Conexao.java
+++ KdeAcai/src/controler/Conexao.java
@@ -2,18 +2,27 @@ package controler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
-	private String caminho;
-	private String login;
-	private String senha;
+	private String caminho = "";
+	private String login = "";
+	private String senha = "";
 	private static Connection con;
 	
-	public Connection conectar() {
-		Class.forName("com.myql.jdbc.Driver");
-		System.out.println("Carregou o driver");
+	public static void conectar() {
+		try {
+			Class.forName("com.myql.jdbc.Driver");
+			System.out.println("Carregou o driver");
+			
+			con = DriverManager.getConnection(caminho, login, senha);
+			System.out.println("Estabeleceu conexão");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Erro ao carregar o Driver");
 		
-		con = DriverManager.getConnection(caminho, login, senha);
-		System.out.println("Estabeleceu conexão");
+		} catch (SQLException e) {
+			System.out.println("Erro ao tentar estabelecer conexão");
+		}
 	}
 }
