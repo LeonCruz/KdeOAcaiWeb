a60a6c47117874711b0e47dc3472d4171c470890
LeonCruz <leon.cruz.teixeira@gmail.com>
2018-03-14 18:02:37 -0300

    Criação do pacote 'DAO'

---
+++ KdeAcai/src/DAO/UsuarioDAO.java
@@ -0,0 +1,12 @@
+package DAO;
+
+import controler.Conexao;
+import controler.ManipulacaoBanco;
+import modelo.Clientes;
+
+public class UsuarioDAO {
+	public static void inserir(Clientes cliente) {
+		String sql;
+		ManipulacaoBanco.manipular(sql);
+	}
+}
