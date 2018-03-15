c90b271353d4a93235f694a784d6f4a177eca71b
ADM <ADM@c-lab04-pc14>
2018-03-15 16:41:40 -0300

    Criação da função 'consultarCliente'

--- KdeAcai/src/DAO/ClientesDAO.java
+++ KdeAcai/src/DAO/ClientesDAO.java
@@ -44,7 +44,7 @@ public class ClientesDAO {
 		}
 	}
 	
-	private static ResultSet consultarCliente(Clientes cliente) {
+	public static ResultSet consultarCliente(Clientes cliente) {
 		String sql  = String.format("SELECT * FROM clientes WHERE login=%s", cliente.getLogin());
 		return ManipulacaoBanco.pegarCliente(sql);
 	}
