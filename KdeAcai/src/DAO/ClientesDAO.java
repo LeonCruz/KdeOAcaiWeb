bb6de73e224695f707c121ad1eb6c00989e86369
ADM <ADM@c-lab04-pc14>
2018-03-15 17:12:29 -0300

    Criação da função 'atualizar'

--- KdeAcai/src/DAO/ClientesDAO.java
+++ KdeAcai/src/DAO/ClientesDAO.java
@@ -15,7 +15,7 @@ public class ClientesDAO {
 			if(resultado.next()) {
 				System.out.println("Login já existe");
 			} else {
-				sql = String.format("INSERT INTO clientes VALUES(%s, %s, %s, %s)", 
+				sql = String.format("INSERT INTO clientes VALUES('%s', '%s', '%s', '%s')", 
 						cliente.getLogin(), cliente.getNome(), cliente.getEmail(), cliente.getSenha());
 				ManipulacaoBanco.manipular(sql);
 			}
@@ -44,8 +44,44 @@ public class ClientesDAO {
 		}
 	}
 	
+	public void atualizar(Clientes cliente) {
+		ResultSet resultado = consultarCliente(cliente);
+		int id;
+		
+		String sql;
+		
+		String novoLogin = cliente.getLogin();
+		String novoNome = cliente.getNome();
+		String novoEmail = cliente.getEmail();
+		String novaSenha = cliente.getSenha();
+		
+		try {
+			if(resultado.next()) {
+				id = resultado.getInt("id");
+				if(!novoLogin.equals("")) {
+					sql = String.format("UPDATE clientes SET login='%s' WHERE id=%d", novoLogin, id);
+				}
+				
+				if(!novoNome.equals("")) {
+					sql = String.format("UPDATE clientes SET nome='%s' WHERE id=%d", novoNome, id);
+				}
+				
+				if(!novoEmail.equals("")) {
+					sql = String.format("UPDATE clientes SET email='%s' WHERE id=%d", novoEmail, id);
+				}
+				
+				if(!novaSenha.equals("")) {
+					sql = String.format("UPDATE clientes SET senha='%s' WHERE id=%d", novaSenha, id);
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	private static ResultSet consultarCliente(Clientes cliente) {
-		String sql  = String.format("SELECT * FROM clientes WHERE login=%s", cliente.getLogin());
+		String sql  = String.format("SELECT * FROM clientes WHERE login='%s'", cliente.getLogin());
 		return ManipulacaoBanco.pegarCliente(sql);
 	}
 }
