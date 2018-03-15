6ebf9fcb4ddb9c8e72a1674a31bd674b834a2787
LeonCruz <leon.cruz.teixeira@gmail.com>
2018-03-15 15:06:35 -0300

    Alteração na função 'deletar'

--- KdeAcai/src/DAO/ClientesDAO.java
+++ KdeAcai/src/DAO/ClientesDAO.java
@@ -9,9 +9,23 @@ import modelo.Clientes;
 
 public class ClientesDAO {
 	public static void inserir(Clientes cliente) {
-		String sql = String.format("INSERT INTO clientes VALUES(%s, %s, %s, %s)", 
-									cliente.getLogin(), cliente.getNome(), cliente.getEmail(), cliente.getSenha());
-		ManipulacaoBanco.manipular(sql);
+		String sql  = String.format("SELECT * FROM clientes WHERE login=%s", cliente.getLogin());
+		ResultSet resultado = ManipulacaoBanco.pegarCliente(sql);
+		
+		try {
+			if(resultado.next()) {
+				System.out.println("Login já existe");
+			} else {
+				sql = String.format("INSERT INTO clientes VALUES(%s, %s, %s, %s)", 
+						cliente.getLogin(), cliente.getNome(), cliente.getEmail(), cliente.getSenha());
+				ManipulacaoBanco.manipular(sql);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 	public static void deletar(Clientes cliente) {
@@ -20,12 +34,16 @@ public class ClientesDAO {
 		int id;
 		
 		try {
-			id = resultado.getInt("id");
-			sql = String.format("DETETE FROM clientes WHERE id=%d", id);
-			System.out.println("Cliente deletado!");
+			if(resultado.next()) {
+				id = resultado.getInt("id");
+				sql = String.format("DETETE FROM clientes WHERE id=%d", id);
+				System.out.println("Cliente deletado!");
+			} else {
+				System.out.println("Login não existe");
+			}
 			
 		} catch (SQLException e) {
-			System.out.println("Não encontrado");
+			e.printStackTrace();
 		}
 	}
 }
