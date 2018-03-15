15cf7d80177255b8a1676e99d20c10b6ff4752c1
LeonCruz <leon.cruz.teixeira@gmail.com>
2018-03-15 17:52:03 -0300

    Criação da função 'deletar'

--- KdeAcai/src/DAO/LojasDAO.java
+++ KdeAcai/src/DAO/LojasDAO.java
@@ -1,5 +1,8 @@
 package DAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import controle.ManipulacaoBanco;
 import modelo.Lojas;
 
@@ -12,6 +15,35 @@ public class LojasDAO {
 				loja.getTelofone(), loja.getTipoFino(), loja.getTipoMedio(), loja.getTipoGrosso());
 		
 		ManipulacaoBanco.manipular(sql);
+		System.out.println("Loja cadastrada");
+	}
+	
+	public static void deletar(Lojas loja) {
+		// Função que deleta uma loja
+		
+		int id = 0;
+		ResultSet resultado = consultarLoja(loja);
+		String sql = String.format("DETELE FROM lojas WHERE id=%d", id);
+		
+		try {
+			if(resultado.next()) {
+				id = resultado.getInt("id");
+				sql = String.format("DETETE FROM lojas WHERE id=%d", id);
+				System.out.println("Loja deletado!");
+			} else {
+				System.out.println("Loja não existe");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static ResultSet consultarLoja(Lojas loja) {
+		// Função que pesquisa no banco uma loja que já existe;
+		
+		String sql  = String.format("SELECT * FROM lojas WHERE login='%s'", loja.getEmail());
+		return ManipulacaoBanco.pegarCliente(sql);
 	}
 	
 }
