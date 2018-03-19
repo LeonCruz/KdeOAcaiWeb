957c6224b9517ab7f9165419aa05e95b8e5f130d
RochaJG <70rd23@gmail.com>
2018-03-19 19:20:29 -0300

    Correção de informações de conexão com o banco

--- KdeAcai/src/controle/Conexao.java
+++ KdeAcai/src/controle/Conexao.java
@@ -5,26 +5,30 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-	private static String caminho = "localhost";
+	private static String caminho = "jdbc:mysql://localhost:3306/kde_acai";
 	private static String login = "root";
-	private static String senha = "root";
+	private static String senha = "ifpa@123";
 	private static Connection con;
 	
-	public static void conectar() {
+	public static Connection conectar() {
 		// Função que realiza a conexeão com o banco
 		
 		try {
-			Class.forName("com.myql.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Carregou o driver");
 			
 			con = DriverManager.getConnection(caminho, login, senha);
-			System.out.println("Estabeleceu conex�o");
+			System.out.println("Estabeleceu conexão");
 			
 		} catch (ClassNotFoundException e) {
 			System.out.println("Erro ao carregar o Driver");
 		
 		} catch (SQLException e) {
 			System.out.println("Erro ao tentar estabelecer conex�o");
+			System.out.println(e);
 		}
+		
+		
+		return con;
 	}
 }
